b3f4d4d9bb43c7006ff37243881b6d7312d907de
stewartcelani <me@stewartcelani.com>
2022-04-13 13:27:08 +0930

    Step 9: Clean up Client code

---
+++ Client/Library/InfiniteRetryPolicy.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace WasmTwoWayLogging.Client.Library
+{
+    public class InfiniteRetryPolicy : IRetryPolicy
+    {
+        public TimeSpan? NextRetryDelay(RetryContext retryContext)
+        {
+            // For the first 5 minutes try to reconnect every 15 seconds and after that try every 60
+            if (retryContext.ElapsedTime < TimeSpan.FromSeconds(300))
+            {
+                return TimeSpan.FromSeconds(15);
+            }
+            else
+            {
+                return TimeSpan.FromSeconds(60);
+            }
+        }
+    }
+}
--- Client/Services/DebugService.cs
+++ Client/Services/DebugService.cs
@@ -5,9 +5,6 @@ namespace WasmTwoWayLogging.Client.Services
     public class DebugService
     {
         private readonly JsConsole _console;
-        public event Func<Task>? OnChange;
-        public bool ConnectToServerConsole = false;
-        public List<string> LogMessages = new List<string>();
 
         public DebugService(JsConsole console)
         {
@@ -15,34 +12,9 @@ namespace WasmTwoWayLogging.Client.Services
             _console = console;
         }
 
-        public async Task AddLogMessage(string logMessage)
+        public async Task HandleLogFromServer(string logMessage)
         {
-            LogMessages.Add(logMessage);
             await _console.LogAsync(logMessage);
-            HandleOnChange();
-        }
-
-        public async Task ClearLogMessages()
-        {
-            Logger.Log.Trace("Top");
-            LogMessages.Clear();
-            await _console.ClearAsync();
-            HandleOnChange();
-        }
-
-        public void ToggleConnectToServerConsole()
-        {
-            Logger.Log.Trace("Top");
-            ConnectToServerConsole = !ConnectToServerConsole;
-            HandleOnChange();
-        }
-
-        private void HandleOnChange()
-        {
-            if (OnChange != null)
-            {
-                OnChange?.Invoke();
-            }
         }
     }
 }
